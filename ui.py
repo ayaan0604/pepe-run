a62b1e78690ea2abce6e64e77a68c64e88e088ce
ayaan0604 <ansariayaan0604@gmail.com>
2026-07-08 03:19:42 +0530

    gameoverscreen done

--- ui.py
+++ ui.py
@@ -143,7 +143,75 @@ class TopArea(Frame):
         self.scoreLabel.place(relx = 0, x= 10, rely = 0)
         self.lifeLabel.place(relx = 1, rely = 0, x = -10, anchor = "ne")
 
+
+class GameOverScreen(Frame):
+    def __init__(self, parent, width, height):
+        super().__init__(
+            parent,
+            width = width,
+            height = height,
+            bd = 2,
+            relief = RAISED
+        )
+
+        self.backgroundImage = ImageLabel(
+            self,
+            location = "assets/gameOver.png",
+            width = width-10,
+            height = height-10
+        )
+        self.backgroundImage.place(x=0 ,y = 0)
+
+
         
+
+        # #restart button
+        self.restart_button=Button(self,
+                            text="Restart",
+                            bg="#022211",
+                            fg="#28ed38",
+                            font=("Lucida Console",18,"bold"),
+                            width= int(width * 0.025),
+                            bd = 0
+                           
+                            )
+
+
+        #score place
+        self.score_display=Label(self,text="0",
+                            width=10,
+                            height=1,
+                            font=("Komika Axis",15,"bold"),
+                            bg="#011228",
+                            padx= 1,
+                            pady= 1,
+                            fg="white"
+                            )
+
+        self.quitButton = Button(self,
+            text = "Quit",
+            bg="#080717",
+            fg="#e8e8e8",
+            font=("Lucida Console",12,"bold"),
+            width= int(width * 0.025),
+            bd = 2
+        )
+
+        self.restart_button.place(relx=0.33, rely=0.805, anchor = "center")
+        self.score_display.place(relx=0.19,rely=0.43)
+        self.quitButton.place(relx = 0.17, rely = 0.9)
+
+        def show(self, score):
+            self.score_display_place.config(text = f'{score}')
+            self.lift()
+        
+        def hide(self):
+            self.lower()
+        
+        
+
+            
+
 
 class App(Tk):
     def __init__(self, width, height, title):
@@ -169,6 +237,8 @@ class Screen:
 
         self.controlHeight=self.windowHeight*0.19
         self.controlWidth=self.windowWidth*0.4
+
+
 
 class Ui:
     def __init__(self):
@@ -208,62 +278,12 @@ class Ui:
 
         
 
-    def setup_score_counter(self):
-        score_counter=Frame(self.window,width=200,height=40,bg="black")
-        score_counter.place(x=0,y=0)
-        score_text=Label(score_counter,text="Score: ",font=("Komika Axis",15,"bold"),bg="black",fg="yellow")
-        self.score_number=Label(score_counter,text="0",font=("Komika Axis",15,"bold"),bg="black",fg="yellow")
-        score_text.place(x=0,y=0)
-        self.score_number.place(x=80,y=0)
-
-    def setup_life_counter(self):
-         #life counter
-        life_counter=Frame(self.window,width=100,height=40,bg="black")
-        life_counter.place(x=self.screen.windowWidth-100,y=0)
-        self.life_text=Label(life_counter,text="🩷"*self.pepe.maxLives,font=("Arial",20),bg="black", fg="red")
-        self.life_text.place(x=0,y=0)
-
+   
     def setup_gameover_screen(self):
-        #gameoverscreen
-        self.game_over_frame=Frame(self.window,width=500,height=300,bd=10,relief=RAISED)
-        self.game_over_frame.place(x=self.screen.windowWidth/2-250,y=self.screen.windowHeight/2-200)
+        self.gameOver = GameOverScreen(self.window, int(self.screen.windowWidth*0.4), int(self.screen.windowHeight*0.8))
 
-        self.gameover_image_raw=Image.open("assets/gameOver.png")
-
-        self.gameover_image_resized=ImageTk.PhotoImage(self.gameover_image_raw.resize((470,275)))
-
-        self.gameover_popup=Label(self.game_over_frame,image=self.gameover_image_resized)
-
-        #restart button
-        self.restart_button=Button(self.game_over_frame,
-                            text="Restart",
-                            bg="#000000",
-                            fg="#28ed38",
-                            font=("Lucida Console",15,"bold"),
-                            width=15,
-                            height=1,
-                            bd=0,
-                            relief=RAISED)
-
-        #restart_button.config(command=restart)
-        # self.game_over_frame.bind("<Return>",restart)
-
-        #score place
-        self.score_display=Label(self.game_over_frame,text="0",
-                            width=10,
-                            height=1,
-                            font=("Komika Axis",15,"bold"),
-                            bg="#151552",
-                            padx= 1,
-                            pady= 1,
-                            fg="white"
-                            )
-
-        self.gameover_popup.place(x=0,y=0)
-        self.restart_button.place(x=55,y=210)
-        self.score_display.place(x=100,y=107)
-
-        self.game_over_frame.lower()
+        self.gameOver.place(relx = 0.5, rely = 0.5, anchor = "center")
+        
 
 
    
@@ -271,13 +291,12 @@ class Ui:
     def setup_ui(self):
         
         
+        self.setup_top_area()
+        
         self.setup_play_area()
 
         self.setup_bottom_area()
 
-       
-
-        self.setup_top_area()
 
         self.setup_gameover_screen()
 
